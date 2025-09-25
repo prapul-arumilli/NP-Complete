0b474963363bcd868f5060cb68c5a3472c7c0437
prapul-arumilli <prapularumilli@gmail.com>
2025-09-25 11:14:48 -0400

    Set up boilerplate

--- npcomplete/src/pages/Explore.js
+++ npcomplete/src/pages/Explore.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import './Explore.css'; // Import the CSS file
 
 function Explore() {
   return (
-    <div className="page-container">
-      <h1>Explore</h1>
-      <p>big things go here</p>
+    <div className="explore-page">
+      {/* Search Bar */}
+      <div className="search-bar-container">
+        <input type="text" placeholder="Search" className="search-bar" />
+      </div>
+
+      {/* Content Grid */}
+      <div className="content-grid">
+        {/* Row 1 */}
+        <div className="content-box pink">
+          <div className="wavy-line"></div>
+        </div>
+        <div className="content-box red">
+          <div className="wavy-line"></div>
+        </div>
+        <div className="content-box large-green">
+          <div className="wavy-line"></div>
+        </div>
+
+        {/* Row 2 */}
+        <div className="content-box light-green">
+          <div className="wavy-line"></div>
+        </div>
+        <div className="content-box purple">
+          <div className="wavy-line"></div>
+        </div>
+        {/* The large green box spans 2 rows */}
+
+        {/* Row 3 */}
+        <div className="content-box light-gray">
+          <div className="wavy-line"></div>
+        </div>
+        <div className="content-box light-blue">
+          <div className="wavy-line"></div>
+        </div>
+      </div>
     </div>
   );
 }
